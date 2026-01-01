1a366554613e577c567a3e0f5df2394516d513f8
yennifer <shiksjayne95@gmail.com>
2026-01-01 12:55:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
+function calculateTax(amount) {
+  return amount * 0.10;
+}
 
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
+
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
+
+function isPalindrome(word) {
+  const reversed = word.split("").reverse().join("");
+  return word === reversed;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discount = originalPrice * (discountPercentage / 100);
+  return originalPrice - discount;
+}
 
 
 
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+
+
+
